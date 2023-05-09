78eecc7a3dae0a09069f5c315bc1cc8ab9b1ed49
Amwaj Alotaibi <132268852+Amwajalotaibi@users.noreply.github.com>
2023-05-09 20:07:58 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,130 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Q1
+
+//        int number1= 125;
+//        int number2=24;
+//        System.out.println("125 + 24 = "+(number1+number2));
+//        System.out.println("125 - 24 = "+(number1-number2));
+//        System.out.println("125 * 24 = "+(number1*number2));
+//        System.out.println("125 / 24 = "+(number1/number2));
+//        System.out.println("125 mod 24 = "+(number1%number2));
+
+
+        // Q2
+
+
+//        String line = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+//        System.out.println(line.toLowerCase());
+
+
+        //  Q3
+
+
+//        String name="Java  Bootcamp";
+//        System.out.println(name.charAt(1));
+
+
+        //Q4
+
+//        Scanner S = new Scanner(System.in);
+//        System.out.println("Enter the number");
+//        int number = S.nextInt();
+//
+//        if (number % 2 == 0) {
+//            System.out.println(1);
+//        }else{
+//            System.out.println(0);
+//        }
+
+
+
+        //Q5
+
+//        Scanner S = new Scanner(System.in);
+//        System.out.println("please  enter Your role");
+//        String role = S.nextLine();
+
+//        if (role==("Admin")) {
+//            System.out.println("welcome Admin ");
+
+//        } else if (role==("superuser")) {
+//            System.out.println("welcome superuser");
+
+//        } else if (role==("user")) {
+//            System.out.println("welcome user");
+//        }
+
+        //Q6
+
+//        int number1 = 5;
+//        int number2 = 10;
+//        int number3 = 15;
+//        int sum = number1 + number2;
+//        if (sum == number3) {
+//            System.out.println("Result is True");
+//        } else {
+//            System.out.println("Result is False");
+//        }
+
+        //Q7
+//
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("input the 1st number: ");
+//        int num1 = in.nextInt();
+
+//        System.out.print("input the 2nd number: ");
+//        int num2 = in.nextInt();
+
+//        System.out.print("input the 3rd number: ");
+//        int num3 = in.nextInt();
+//
+//        if (num1 > num2 && num1 > num3) {
+//            System.out.println("The greatest: " + num1);
+//
+//    } else if (num2 > num1 && num2 > num3) {
+//            System.out.println("The greatest: " + num2);
+//
+//        } else if (num3 > num1 && num3 > num2) {
+//            System.out.println("The greatest: " + num3);
+//        }
+
+
+        //Q8
+
+//        Scanner S=new Scanner(System.in);
+//        System.out.println("please enter number");
+//        int day =S.nextInt();
+//
+//        switch (day) {
+//            case 1:
+//                System.out.println("saturday");
+//                break;
+//            case 2:
+//                System.out.println("sunday");
+//                break;
+//            case 3:
+//                System.out.println("wednesday");
+//                break;
+//            case 4:
+//                System.out.println(" tuesday");
+//                break;
+//            case 5:
+//                System.out.println("thursday");
+//                break;
+//            case 6:
+//                System.out.println("monday");
+//                break;
+//            case 7:
+//                System.out.println("friday");
+//                break;
+//        }
+//
+
+
+
+    }
+}
